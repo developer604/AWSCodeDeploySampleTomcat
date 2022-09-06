fcb8d78024291c476fe4e281c520788ebec7d79a
developer604 <78541507+developer604@users.noreply.github.com>
2022-09-06 18:20:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-aws s3 cp s3://firstbuild/firstbuildproject/LoginWebApp-1.war /tmp
+aws s3 cp s3://firstbuild/buildone/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /opt/tomcat/webapps/LoginWebApp-1.war
